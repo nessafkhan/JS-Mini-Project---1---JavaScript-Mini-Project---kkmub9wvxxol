c6693e7c715e4c5306341b13b848754e4f1c0746
nessaf khan <nessafkhan.kn@gmail.com>
2022-05-06 20:46:11 +0530

    highscores added to localstorage

--- End/end.js
+++ End/end.js
@@ -2,7 +2,11 @@ const showScore = document.getElementById('finalScore');
 const mostRecentScore = localStorage.getItem('mostRecentScore');
 const username = document.getElementById('username');
 const saveScoreBtn = document.getElementById('saveScoreBtn');
+
 showScore.innerText = mostRecentScore;
+
+const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
+const MAX_HIGH_SCORE = 5;
 
 username.addEventListener('keyup', () => {
     saveScoreBtn.disabled = !username.value;
@@ -10,4 +14,15 @@ username.addEventListener('keyup', () => {
 
 saveHighScore = e =>{
     e.preventDefault();
+
+    const score = {
+        score: mostRecentScore,
+        name: username.value,
+    };
+    highScores.push(score);
+    highScores.sort( (a,b) => b.score - a.score);
+    highScores.splice(5);
+
+    localStorage.setItem('highScore', JSON.stringify(highScores));
+    window.location.assign('/');
 };
